badaf5315d04d55b26a3ed0e3b1bd4753c90ac1e
Leslie Pajuelo <LPajuelo@walmartlabs.com>
2016-07-04 14:28:06 -0700

    Add annotation support for KairosDB

--- module.js
+++ module.js
@@ -1,20 +1,24 @@
 define([
-  './datasource',
-  './query_ctrl'
-],
-function(KairosDBDatasource, KairosDBQueryCtrl) {
-  'use strict';
+    './datasource',
+    './query_ctrl'
+  ],
+  function(KairosDBDatasource, KairosDBQueryCtrl) {
+    'use strict';
 
-  var KairosDBConfigCtrl = function() {}
-  KairosDBConfigCtrl.templateUrl = "partials/config.html";
+    var KairosDBConfigCtrl = function() {}
+    KairosDBConfigCtrl.templateUrl = "partials/config.html";
 
-  var KairosDBQueryOptionsCtrl = function() {}
-  KairosDBQueryOptionsCtrl.templateUrl = "partials/query.options.html";
+    var KairosDBQueryOptionsCtrl = function() {}
+    KairosDBQueryOptionsCtrl.templateUrl = "partials/query.options.html";
 
-  return {
-    'Datasource': KairosDBDatasource,
-    'QueryCtrl': KairosDBQueryCtrl,
-    'ConfigCtrl': KairosDBConfigCtrl,
-    'QueryOptionsCtrl': KairosDBQueryOptionsCtrl
-  };
-});
+    var KairosDBAnnotationsQueryCtrl = function() {}
+    KairosDBAnnotationsQueryCtrl.templateUrl = "partials/annotations.editor.html";
+
+    return {
+      'Datasource': KairosDBDatasource,
+      'QueryCtrl': KairosDBQueryCtrl,
+      'ConfigCtrl': KairosDBConfigCtrl,
+      'QueryOptionsCtrl': KairosDBQueryOptionsCtrl,
+      'AnnotationsQueryCtrl': KairosDBAnnotationsQueryCtrl
+    };
+  });
